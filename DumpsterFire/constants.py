b4279f7941542b16753e5f25903485502cf38c19
cheeseburger4 <147112825+cheeseburger4@users.noreply.github.com>
2024-01-26 15:13:30 -0800

    robot pivot

--- DumpsterFire/constants.py
+++ DumpsterFire/constants.py
@@ -3,3 +3,7 @@ from enum import Enum
 class MotorIDs(Enum):
     PIVOTMOTOR = 0
     INTAKEMOTOR = 1
+
+class MotorConstants(Enum):
+    PIVOTSPEED = .5
+    INTAKESPEED = .8
--- DumpsterFire/subsystems/intake.py
+++ DumpsterFire/subsystems/intake.py
@@ -6,22 +6,32 @@ class IntakeAndPivot(Subsystem):
     
     def __init__(self):
         super().__init__()
+        
         self.intakeMotor = phoenix6.hardware.TalonFX(MotorIDs.INTAKEMOTOR)
         self.pivotMotor = phoenix6.hardware.TalonFX(MotorIDs.PIVOTMOTOR)
         self.choking = False
 
         CommandScheduler.getInstance().registerSubsystem(self)
 
+
     #####[[ INTAKE FUNCTIONS ]]#####
 
+
     def spit(self) -> None: # drop note
-        self.intakeMotor.set_control(phoenix6.controls.DutyCycleOut(1.0))
+        self.intakeMotor.set_control(phoenix6.controls.DutyCycleOut(MotorConstants.INTAKESPEED))
+
 
     def swallow(self) -> None: # intake note
-        self.intakeMotor.set_control(phoenix6.controls.DutyCycleOut(-1.0))
+        self.intakeMotor.set_control(phoenix6.controls.DutyCycleOut(-MotorConstants.INTAKESPEED))
+
+
+    def choke(self) -> None: # hold note
+        self.intakeMotor.set_control(phoenix6.controls.DutyCycleOut(0))
+
 
     def hasNote(self) -> bool: # use beam break to see if note is inside intake
         pass
+
 
     def periodic(self) -> None: # update whether the robot has the note or not
         if self.hasNote():
@@ -29,13 +39,22 @@ class IntakeAndPivot(Subsystem):
         else:
             self.choking = False
 
+
     #####[[ PIVOT FUNCTIONS ]]#####
             
-    def pivot(self) -> None:
-        pass
+            
+    def pivotDownwards(self) -> None: # point intake downwards
+        self.pivotMotor.set_control(phoenix6.controls.DutyCycleOut(MotorConstants.PIVOTSPEED))
+
+
+    def pivotUpwards(self) -> None: # point intake upwards
+        self.pivotMotor.set_control(phoenix6.controls.DutyCycleOut(-MotorConstants.PIVOTSPEED))
+
+
+    def pivotStop(self) -> None: # stop intake pivot
+        self.pivotMotor.set_control(phoenix6.controls.DutyCycleOut(0))
 
     
-
 
 
 
