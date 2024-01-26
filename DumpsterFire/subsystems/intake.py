cbc13c4eadafde54baa75ae6806f462d9f1f35fb
cheeseburger4 <147112825+cheeseburger4@users.noreply.github.com>
2024-01-26 13:13:26 -0800

    organization + choking

--- DumpsterFire/subsystems/intake.py
+++ DumpsterFire/subsystems/intake.py
@@ -8,23 +8,30 @@ class IntakeAndPivot(Subsystem):
         super().__init__()
         self.intakeMotor = phoenix6.hardware.TalonFX(MotorIDs.INTAKEMOTOR)
         self.pivotMotor = phoenix6.hardware.TalonFX(MotorIDs.PIVOTMOTOR)
+        self.choking = False
 
         CommandScheduler.getInstance().registerSubsystem(self)
 
-    def spit(self) -> None:
+    #####[[ INTAKE FUNCTIONS ]]#####
+
+    def spit(self) -> None: # drop note
         self.intakeMotor.set_control(phoenix6.controls.DutyCycleOut(1.0))
 
-    def periodic(self) -> None:
-        pass
-        
-        # Check if note is swallowed via beam break
-        # if yes, self.choking = True
-        # else, self.choking = False
-
-    def swallow(self) -> None:
+    def swallow(self) -> None: # intake note
         self.intakeMotor.set_control(phoenix6.controls.DutyCycleOut(-1.0))
 
-    def hasNote(self) -> bool:
+    def hasNote(self) -> bool: # use beam break to see if note is inside intake
+        pass
+
+    def periodic(self) -> None: # update whether the robot has the note or not
+        if self.hasNote():
+            self.choking = True
+        else:
+            self.choking = False
+
+    #####[[ PIVOT FUNCTIONS ]]#####
+            
+    def pivot(self) -> None:
         pass
 
     
