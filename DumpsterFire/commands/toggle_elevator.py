fa36ab94c7936e4969e3a962c82f4d74d14e7e8c
CleverDude88 <88858289+Glatia@users.noreply.github.com>
2024-01-27 11:30:32 -0800

    Created basic Elevator/Elevator Command files

---
+++ DumpsterFire/commands/toggle_elevator.py
@@ -0,0 +1,17 @@
+import typing
+import commands2
+from subsystems.elevator import Elevator
+
+
+class ToggleElevator(commands2.CommandBase):
+
+    def __init__(self, elevator: Elevator, button: typing.Callable([], float)):
+
+        self.button = button
+        self.elevator = elevator
+
+        self.addRequirements([self.elevator])
+
+        
+        
+
--- DumpsterFire/subsystems/elevator.py
+++ DumpsterFire/subsystems/elevator.py
@@ -1,1 +1,29 @@
+import phoenix6
+from constants import * 
+from commands2 import CommandScheduler, Subsystem
+
+class Elevator(Subsystem):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.elevatorMotor = phoenix6.hardware.TalonFX(MotorIDs.ELEVATORMOTOR)
+
+        CommandScheduler.getInstance().registerSubsystem(self)
+
+
+    #####[[ ELEVATOR FUNCTIONS ]]#####
+            
+             
+    def Downwards(self) -> None: # Move elevator down
+        self.elevatorMotor.set_control(phoenix6.controls.DutyCycleOut(MotorConstants.ELEVATORSPEED))
+
+
+    def Upwards(self) -> None: # Elevator goes up
+        self.elevatorMotor.set_control(phoenix6.controls.DutyCycleOut(-MotorConstants.ELEVATORSPEED)) 
+
+
+    
+
+
 
