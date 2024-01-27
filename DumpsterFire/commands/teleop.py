36fcf5a89f4508d2c8691901d3b6b4a66538c23b
TheSavageDemon <147454543+TheSavageDemon@users.noreply.github.com>
2024-01-27 14:42:34 -0800

    Added commands and changed names
    I added commands for taking and pushing. Hopefully only has a couple errors.
    
    Also I changed spit, swallow, and choke to disencumber, consume, and restrain cus James is a coward

--- DumpsterFire/commands/teleop.py
+++ DumpsterFire/commands/teleop.py
@@ -1,1 +1,23 @@
+import commands2
+import constants
+from subsystems.intake import Intake
 
+class DriveControllerDefault(commands2.CommandBase):
+    def __init__(self, Intake: Intake,
+
+    def initialize(self) -> None:
+        super().__init__()
+        self.intake = Intake
+        self.x = x
+        self.y = y
+        self.rightx = rightx
+        self.leftTrigger = leftTrigger
+        self.rightTrigger = rightTrigger
+
+    def execute(self) -> None:
+        
+        if self.leftTrigger():
+            self.intake.consume(self)
+
+        if self.rightTrigger():
+            self.intake.disencumber(self)
