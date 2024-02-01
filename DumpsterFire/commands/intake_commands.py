9a4536168e6551f929d066987a50c72cf74c09db
PickleFace5 <88858289+Glatia@users.noreply.github.com>
2024-01-31 18:16:46 -0800

    working intake!!!!!!!!!!!!!!!! (i am micah not matthew matthew was USELESS!)

--- DumpsterFire/commands/intake_commands.py
+++ DumpsterFire/commands/intake_commands.py
@@ -2,13 +2,48 @@ import wpilib
 import commands2
 from subsystems.intake import IntakeAndPivot
 
-class FeederTest(commands2.CommandBase):
 
-    def __init__(self, IntakeAndPivot):
+class FeederTest(commands2.Command):
+
+    def __init__(self, IntakeAndPivot : IntakeAndPivot):
         self.intake = IntakeAndPivot
         self.addRequirements(self.intake)
 
+    def initialize(self):
+        return super().initialize()
+
     def execute(self):
-        
         self.intake.consume()
 
+    def isFinished(self) -> bool:
+        return False
+
+class FeederTestDrop(commands2.Command):
+
+    def __init__(self, IntakeAndPivot : IntakeAndPivot):
+        self.intake = IntakeAndPivot
+        self.addRequirements(self.intake)
+
+    def initialize(self):
+        return super().initialize()
+
+    def execute(self):
+        self.intake.disencumber()
+
+    def isFinished(self) -> bool:
+        return False
+    
+class FeederTestStop(commands2.Command):
+
+    def __init__(self, IntakeAndPivot : IntakeAndPivot):
+        self.intake = IntakeAndPivot
+        self.addRequirements(self.intake)
+
+    def initialize(self):
+        return super().initialize()
+
+    def execute(self):
+        self.intake.hold()
+
+    def isFinished(self) -> bool:
+        return False
--- DumpsterFire/constants.py
+++ DumpsterFire/constants.py
@@ -1,20 +1,20 @@
 from enum import Enum
 
-class MotorIDs(Enum):
+class MotorIDs:
     PIVOTMOTOR = 0
     INTAKEMOTOR = 1
     ELEVATORMOTOR = 2
 
-class IntakeConstants(Enum):
+class IntakeConstants:
     PIVOTSPEED = .5
-    INTAKESPEED = .8
+    INTAKESPEED = .2
 
-class ElevatorConstants(Enum):
+class ElevatorConstants:
     ELEVATORTOGGLE = True
     TOPPOSITION = 0 # make this a range for the top position
     BOTTOMPOSITION = 0 # make this a rannge for the bottom position
     FEEDFORWARD = 0 # change this for whatever the feedforward will be
 
-class ExternalConstants(Enum):
+class ExternalConstants:
     DRIVERCONTROLLER = 0
     FUNCTIONSCONTROLLER = 1
