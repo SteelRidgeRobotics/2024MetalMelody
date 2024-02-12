384519e6cf1fc129ddfda756652d996c2c68d00c
PickleFace5 <caden.dalley@icloud.com>
2024-02-11 20:29:16 -0800

    ok i'm pretty sure we don't need 8 lines for explaining deadband-

--- commands/drive.py
+++ commands/drive.py
@@ -50,13 +50,6 @@ class DriveByController(Command):
         return False
     
 def deadband(value: float, band: float):
-    """
-    value is the value we want to deadband
-    the band is the abs value the value can not be less than
-    """
-    # this makes sure that joystick drifting is not an issue.
-    # It takes the small values and forces it to be zero if smaller than the 
-    # band value
     if fabs(value) <= band:
         return 0
     else:
