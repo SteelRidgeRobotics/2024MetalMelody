780b9cbaafaecf2893d70a6dc16f5b6b4867aa7c
PickleFace5 <caden.dalley@icloud.com>
2024-02-15 21:30:40 -0800

    Shrinked elevator.py

--- subsystems/elevator.py
+++ subsystems/elevator.py
@@ -1,20 +1,19 @@
-import phoenix6
-from phoenix6.controls import MotionMagicDutyCycle, Follower
-from constants import * 
+from phoenix6.configs import TalonFXConfiguration
+from phoenix6.controls import Follower, MotionMagicDutyCycle
+from phoenix6.hardware import TalonFX
 from commands2 import Subsystem
+from constants import * 
 
 class Elevator(Subsystem):
     
     def __init__(self):
         super().__init__()
-        self.elevatorMotorRight = phoenix6.hardware.TalonFX(MotorIDs.ELEVATORMOTOR_RIGHT)
-        self.elevatorMotorLeft = phoenix6.hardware.TalonFX(MotorIDs.ELEVATORMOTOR_LEFT)
-        elevator_config = phoenix6.configs.TalonFXConfiguration()
+        self.elevatorMotorRight = TalonFX(MotorIDs.ELEVATORMOTOR_RIGHT)
+        self.elevatorMotorLeft = TalonFX(MotorIDs.ELEVATORMOTOR_LEFT)
+        elevator_config = TalonFXConfiguration()
         elevator_config.slot0.with_k_p(ElevatorConstants.kP)
         elevator_config.motion_magic.with_motion_magic_acceleration(1).with_motion_magic_cruise_velocity(ElevatorConstants.MOTIONMAGICVELOCITY).with_motion_magic_jerk(ElevatorConstants.MOTIONMAGICJERK)
-        elevator_config.current_limits.supply_current_limit = ElevatorConstants.CURRENTSUPPLYLIMIT
-        elevator_config.current_limits.supply_current_limit_enable = ElevatorConstants.CURRENTSUPPLYLIMIT
-        
+        elevator_config.current_limits.with_supply_current_limit_enable(True).with_supply_current_limit(ElevatorConstants.CURRENTSUPPLYLIMIT)
         self.elevatorMotorRight.configurator.apply(elevator_config)
         self.elevatorMotorLeft.configurator.apply(elevator_config)
         
@@ -23,11 +22,7 @@ class Elevator(Subsystem):
         self.elevatorMotorLeft.set_control(Follower(self.elevatorMotorRight.device_id, True))
         
     def up(self) -> None:
-        self.setTo(ElevatorConstants.TOPPOSITION)
+        self.elevatorMotorRight.set_control(MotionMagicDutyCycle(ElevatorConstants.TOPPOSITION))
         
     def below(self) -> None:
-        self.setTo(ElevatorConstants.BOTTOMPOSITION)
-        
-    def setTo(self, pos: float) -> None:
-        self.elevatorMotorRight.set_control(MotionMagicDutyCycle(pos))
-    
+        self.elevatorMotorRight.set_control(MotionMagicDutyCycle(ElevatorConstants.BOTTOMPOSITION))
