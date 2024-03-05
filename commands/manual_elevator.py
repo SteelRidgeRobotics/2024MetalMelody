6836594f7ff58644521280637e9aea19cda4d7c0
PickleFace5 <caden.dalley@icloud.com>
2024-03-04 19:06:02 -0800

    ManualElevator now VoltageOut

--- commands/manual_elevator.py
+++ commands/manual_elevator.py
@@ -1,7 +1,7 @@
 from commands2 import Command
 from constants import *
 from frc6343.controller.deadband import deadband
-from phoenix6.controls import DutyCycleOut
+from phoenix6.controls import VoltageOut
 from subsystems.elevator import Elevator
 from wpilib import XboxController
 
@@ -16,10 +16,10 @@ class ManualElevator(Command):
         self.addRequirements(self.elevator)
 
     def execute(self):
-        self.elevator.setDutyCycle(DutyCycleOut(self.getTriggerCombinedValue()))
+        self.elevator.setDutyCycle(VoltageOut(self.getTriggerCombinedValue() * 6))
 
     def end(self, interrupted: bool):
-        self.elevator.setDutyCycle(DutyCycleOut(0))
+        self.elevator.setDutyCycle(VoltageOut(0))
 
     def getTriggerCombinedValue(self) -> float:
         return -deadband(self.controller.getLeftTriggerAxis(), ExternalConstants.TRIGGER_DEADBAND)
