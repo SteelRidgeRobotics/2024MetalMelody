c3d49d3db6c77a25217bb5a1d6f044b99e8d29d9
cheeseburger4 <147112825+cheeseburger4@users.noreply.github.com>
2024-03-27 16:37:23 -0700

    Fixed

--- commands/drive.py
+++ commands/drive.py
@@ -23,9 +23,9 @@ class DriveByController(Command):
         self.mode = DriveModes.FIELD_RELATIVE
     
     def execute(self) -> None:
-        translation_x = -(self.controller.getLeftY()) ** 3 * SwerveConstants.k_max_module_speed
-        translation_y = -(self.controller.getLeftX()) ** 3 * SwerveConstants.k_max_module_speed
-        rotation = -(self.controller.getRightX()) ** 3 * SwerveConstants.k_max_rot_rate
+        translation_x = (-self.controller.getLeftY() ** 3) * SwerveConstants.k_max_module_speed
+        translation_y = (-self.controller.getLeftX() ** 3) * SwerveConstants.k_max_module_speed
+        rotation = (-self.controller.getRightX() ** 3) * SwerveConstants.k_max_rot_rate
         
         slowdown_mult = 1
         if self.controller.getRightBumper():
