b3451b2b51df8f8f35692f89aebea8d95aff507f
PickleFace5 <caden.dalley@icloud.com>
2024-05-06 18:24:30 -0700

    Creating indexer skeleton (spooky)

---
+++ subsystems/indexer.py
@@ -0,0 +1,19 @@
+from commands2 import Subsystem
+
+class Indexer(Subsystem):
+    
+    def __init__(self) -> None:
+        super().__init__()
+        
+        # MOTOR STUFF HERE
+        
+    def sendToShooter(self) -> None:
+        """Sends the current held note into le speaker!!!
+        """
+        
+    # Perhaps
+    # def sendToIntake(self) -> None:
+        """Sends the current held note back into le intake!!!
+        """
+        
+    
