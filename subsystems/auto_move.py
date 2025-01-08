f2c895f70697fa6580f6c677740fe1f82bb4348b
TheSavageDemon <147454543+TheSavageDemon@users.noreply.github.com>
2025-01-07 17:19:58 -0800

    Skibidi Distance 2: The Distancing

--- subsystems/auto_move.py
+++ subsystems/auto_move.py
@@ -20,5 +20,7 @@ class AutoMove(Subsystem):
     def calculateDistance(self, april_tag_id):
         robot_pose = LimelightHelpers.get_botpose_estimate_wpiblue_megatag2("").pose.translation()
         tag_pose = Constants.k_apriltag_layout.getTagPose(april_tag_id).toPose2d().translation()
-        robot_to_tag_dist = math.sqrt(math.fabs(robot_pose.x-tag_pose.x)**2+math.fabs(robot_pose.y-tag_pose.y)**2)
+        distance_x = robot_pose.x-tag_pose.x
+        distance_y = robot_pose.y-tag_pose.y
+        #robot_to_tag_dist = math.sqrt(math.fabs(robot_pose.x-tag_pose.x)**2+math.fabs(robot_pose.y-tag_pose.y)**2)
         
