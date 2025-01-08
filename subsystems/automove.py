3bfc6fc1fd795fe1d78433293c3d2992c20559c6
TheSavageDemon <147454543+TheSavageDemon@users.noreply.github.com>
2025-01-07 16:37:22 -0800

    Skibidi command

--- subsystems/automove.py
+++ subsystems/automove.py
@@ -1,4 +1,16 @@
 from commands2 import Subsystem
+import ntcore
+from limelight import LimelightHelpers
 
 class Automove(Subsystem):
-    
+
+    def __init__(self):
+        super().__init__()
+        self.setName("Automove")
+        self.ntInstance = ntcore.NetworkTableInstance.getDefault()
+
+    def periodic(self) -> None:
+        self.calculateDistance()
+
+    def calculateDistance(self):
+        robot_pose = LimelightHelpers.
