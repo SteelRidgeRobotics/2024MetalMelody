e56f06b4efaa8f399b531f48a0a09603e2b0e6e4
Caden Dalley <caden.dalley@icloud.com>
2025-01-11 00:03:25 -0800

    Update tests

--- tests/pyfrc_test.py
+++ tests/pyfrc_test.py
@@ -1,6 +1,21 @@
-'''
-    This test module imports tests that come with pyfrc, and can be used
-    to test basic functionality of just about any robot.
-'''
+import pytest
+import typing
 
-from pyfrc.tests import *
+if typing.TYPE_CHECKING:
+    from pyfrc.test_support.controller import TestController
+
+@pytest.mark.filterwarnings("ignore")
+def test_impatient(control: "TestController"):
+
+    with control.run_robot():
+        # Run disabled for a second
+        control.step_timing(seconds=1, autonomous=True, enabled=False)
+
+        # Run autonomous + enabled for a second
+        control.step_timing(seconds=1, autonomous=True, enabled=True)
+
+        # You get the idea
+        control.step_timing(seconds=1, autonomous=False, enabled=False)
+
+        # Teleop
+        control.step_timing(seconds=1, autonomous=False, enabled=True)
